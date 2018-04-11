d7cd1228ad089334ede974030fdece5650ba09e4
Minshu Xie <jiaojiaosakura@gmail.com>
2018-04-11 17:28:29 -0400

    resubmit

--- js/popcorn-data-from-google.js
+++ js/popcorn-data-from-google.js
@@ -3,34 +3,34 @@ var loadPopcorn = function(){
     // Create a popcporn instance by calling Popcorn("#id-of-the-media-element")
     var pop = Popcorn("#media");
 
-    // Next we need to identify which Google spreadsheet we're going to want to use.  
-    // You'll need to change the key to match your spreadsheet.  Remember, this is the 
+    // Next we need to identify which Google spreadsheet we're going to want to use.
+    // You'll need to change the key to match your spreadsheet.  Remember, this is the
     // part in the spreadsheet URL that comes between "key=" and "&"
     // It's very important, though, to keep the same column headers as in our example spreadsheet!
-    var public_spreadsheet_key = 'https://docs.google.com/spreadsheets/d/1pL_Lj62_ZcW7iawTCQ_5BQsmdynCtC8y5BCNy3k2LOM/pubhtml?gid=0&single=true';
-    
-    // now we are going to use the tabletop.js library, which was called in our 
-    // HTML file, to grab the date from the spreadsheet and process it so that 
-    // popcorn can use it.  
+ var public_spreadsheet_key = 'https://docs.google.com/spreadsheets/d/1UVN1-nNlZ449fTmswUFAd6tTgcOvSnSEnd6GvQK4unI/edit?usp=sharing';//  'https://docs.google.com/spreadsheets/d/1pL_Lj62_ZcW7iawTCQ_5BQsmdynCtC8y5BCNy3k2LOM/pubhtml?gid=0&single=true';
+
+    // now we are going to use the tabletop.js library, which was called in our
+    // HTML file, to grab the date from the spreadsheet and process it so that
+    // popcorn can use it.
     var mytables = Tabletop.init( { key: public_spreadsheet_key,
                                     callback: processInfo ,
                                     simpleSheet: false } );
-    // if you're having problems with this, you can uncomment the next line and 
+    // if you're having problems with this, you can uncomment the next line and
     // look in the browser's console to see if the data looks like it's supposed to.
     // console.log(mytables.data);
 
     // This is our "callback function".  When tabletop grabs the spreadsheet data,
     // it gets sent here and processed.  We take advantage of this feature of tabletop
-    // to have our events get automatically created.  
+    // to have our events get automatically created.
     function processInfo(sheets) {
         console.log(sheets);
-        // in this example "simpleSheet" is turned OFF, so 
-        // we need to select only the data in the sheet we're 
-        // looking for.  In my example the sheet containing popcorn-data is 
+        // in this example "simpleSheet" is turned OFF, so
+        // we need to select only the data in the sheet we're
+        // looking for.  In my example the sheet containing popcorn-data is
         // called "popcornSheet".  If you name your sheet something else this won't
-        // work.  
+        // work.
         var data = sheets["popcorn-data"].all();
-        // this loop runs once for each row in the spreadsheet. 
+        // this loop runs once for each row in the spreadsheet.
         for (var i in data) {
             // use the plugin column to create a popcorn event of type "plugin"
             pop[data[i].plugin] ({
@@ -49,15 +49,15 @@ var loadPopcorn = function(){
 
 
             // this is another debugging line -- if things aren'tworking right you can uncomment it & see what's going wrong
-            // in the console.  
+            // in the console.
             console.log(pop);
         };
     }
-    
+
     // one more function that allows us to write start and end times in human-friendly format like 1:05:14.6
     // stolen from Atul's cool instappin' project from 2011:
     // https://github.com/toolness/instapoppin/blob/gh-pages/instapoppin.js
-    // added some comments so you can follow it. 
+    // added some comments so you can follow it.
     function getTimestampInSeconds(ts) {
         if (ts.length == 0)
             return 0;
@@ -75,21 +75,21 @@ var loadPopcorn = function(){
         case 1:
             // It's a pure seconds-based offset.
             return realTimeParts[0];
-            
+
         case 2:
             // It's in MM:SS format.
             return realTimeParts[0] * 60 + realTimeParts[1];
-            
+
         case 3:
             // It's in HH:MM:SS format.
             return ((realTimeParts[0] * 60) + realTimeParts[1]) * 60 +
                 realTimeParts[2];
-            
+
         default:
             throw new ParseError('too many colons in time: ' + ts);
         }
     }
-    
+
     // play the video right away
     // pop.play();
 
